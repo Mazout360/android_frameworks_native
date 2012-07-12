4ba5c3fe725e131c590a5887bd8f5e3645f2b2dd
Emerson Pinter <dev@pinter.com.br>
2012-07-11 23:47:20 -0300

    graphics: Add directives for compatibility with older EGL blobs (2/2)
    Port from CM9.
    Original author Ricardo Cerqueira (commit
    fc496c0607d0d2863ff9587019e890ab806ea49d)
    
    Change-Id: If30f10f1d4b26547d1219e2ba05f6a831b8fbff8

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -121,6 +121,15 @@ status_t GraphicBufferAllocator::alloc(uint32_t w, uint32_t h,
     }
 #endif
 
+#ifdef MISSING_EGL_PIXEL_FORMAT_YV12
+    if (format == HAL_PIXEL_FORMAT_YV12) {
+	format = HAL_PIXEL_FORMAT_RGBX_8888;
+    }
+    if (usage & GRALLOC_USAGE_EXTERNAL_DISP) {
+	usage ^= GRALLOC_USAGE_EXTERNAL_DISP;
+    }
+#endif
+
 #ifdef QCOM_HARDWARE
     if(bufferSize) {
         err = mAllocDev->allocSize(mAllocDev, w, h,
